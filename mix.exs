ee0687d7d1b9d87c4424354b79075e88ab6f659d
Victor Borja <vborja@apache.org>
2018-03-06 15:50:26 -0600

    hex

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Deco.MixProject do
   def project do
     [
       app: :deco,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
